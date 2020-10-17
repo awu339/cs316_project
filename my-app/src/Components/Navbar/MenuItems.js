190c2e2beb26bed6990e20a7ae3e6e00c318fab2
Andrew Wu <andrew.wu724@duke.edu>
2020-10-16 23:19:04 -0400

    garbage

---
+++ my-app/src/Components/Navbar/MenuItems.js
@@ -0,0 +1,31 @@
+const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Contacts',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
