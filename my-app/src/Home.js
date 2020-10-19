c63e58a9ce6f5e00041baa2b2d7a8086c205f591
Andrew Wu <andrew.wu724@duke.edu>
2020-10-19 15:36:57 -0400

    remove useless packages

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div>
       <Nav/>
       <h1>User movies</h1>
-      <img src = "/logo192.png" alt ="image"/>
+      <img src = "titanic_movieposter.jpg" alt ="image"/>
       <h1>Recommended movies</h1>
     
     </div>
--- my-app/src/LoginPage.js
+++ my-app/src/LoginPage.js
@@ -6,7 +6,6 @@ import { Button, Form, FormGroup, Label, Input }
 import { Redirect } from  'react-router-dom';
 import { Link } from 'react-router-dom';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import { FacebookLoginButton } from 'react-social-login-buttons';
 import Home from './Home';
 class LoginPage extends Component {
 
